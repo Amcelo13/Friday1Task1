7040325b374163a5f75be1b99cd7172a1003f5d6
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-06-19 10:46:59 +0530

    Update Box.jsx

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -29,6 +29,7 @@ const tailFormItemLayout = {
     },
   },
 };
+
 import React, { useEffect, useState } from "react";
 import db from "../data/Mock.json";
 import "./Box.scss";
@@ -60,13 +61,14 @@ function Box() {
   const [form] = Form.useForm();
 
   const [isaddopen, setIsAddOpen] = useState(false);
-  const [index ,setIndex] = useState(-1)
-  const [mode ,setMode] = useState('')
+  const [index, setIndex] = useState(-1);
+  const [mode, setMode] = useState("");
 
-  const openAdd = (i,m) => {
+  const openAdd = (i, m) => {
     setIsAddOpen(true);
-    setIndex(i)
-    setMode(m)
+    setIndex(i);
+    setMode(m);
+    editFill();
   };
   const handleAddOk = () => {
     setIsAddOpen(false);
@@ -78,13 +80,12 @@ function Box() {
   //onfinish
   const handleAdd = (values) => {
     console.log(values);
-    updateProduct(values, mode, index)
+    updateProduct(values, mode, index);
     setIsAddOpen(false);
   };
   const [realStore, setRealStore] = useState([]);
   const [myDraft, setDraft] = useState([]);
-  // const useType = localStorage.getItem('useType');
-  
+
   useEffect(() => {
     if (localStorage.getItem("realStore")) {
       let data = JSON.parse(localStorage.getItem("realStore"));
@@ -94,166 +95,189 @@ function Box() {
       let dummydraft = JSON.parse(localStorage.getItem("draft"));
       setDraft([...dummydraft]);
     }
-  },);  //infinite loop
+      //Prepopulating the real store
+    
+  }, [realStore, , myDraft]); //infinite loop
 
-  const handleDelete = (i,mode) => {
-    handelDeleteItem(i,mode);
+  const handleDelete = (i, mode) => {
+    handelDeleteItem(i, mode);
   };
+  const editFill = () => {
+    let data = JSON.parse(localStorage.getItem(mode));
+    form.setFieldsValue({
+      product_name: data[index].name,
+      product_price: data[index].price,
+      description: data[index].description,
+    });
+  };
+
 
   return (
     <>
       <h1 style={{ paddingTop: "2rem" }}> Products</h1>
       <div className="boxes">
-        {realStore.map((e, i) => {
-          if (e.name !== ''){
-          return (
-            <div key={i}>
-              <div className="box">
-                <img
-                  src="https://i.pinimg.com/originals/a3/29/66/a32966c2ab5ba98a7332a72eb1953c48.jpg"
-                  alt=""
-                  className="imgca"
-                />
-                <div className="hash">
-                  <h3>{e.name}</h3>
-                  <h4>₹{e.price}</h4>
-                </div>
 
-                <p>{e.description}</p>
-                <button onClick={()=>openAdd(i,"realStore")}>Edit</button>
-              <Modal
-                title="Add Product Details"
-                open={isaddopen}
-                onOk={handleAddOk}
-                footer={null}
-                onCancel={handleAddCancel}
-                style={{ paddingTop: "2rem" }}
-              >
-                <Form
-                  name="normal_login"
-                  className="login-form"
-                  initialValues={{
-                    remember: true,
-                  }}
-                  onFinish={(values) => handleAdd(values)}
-                >
-                  <div className="addcontainer">
-                    <Form.Item
-                      label="Image      "
-                      valuePropName="fileList"
-                      getValueFromEvent={normFile}
-                    >
-                      {" "}
-                      <center>
-                        <Upload action="/upload.do" listType="picture-card">
-                          <div>
-                            <PlusOutlined />
-                            <div
-                              style={{
-                                marginTop: 8,
-                              }}
-                            >
-                              Upload
-                            </div>
-                          </div>
-                        </Upload>
-                      </center>
-                    </Form.Item>
-                    <Form.Item
-                      name="product_name"
-                      label="Name"
-                      tooltip="What do you want others to call you?"
-                      rules={[
-                        {
-                          message: "Please input your nickname!",
-                          whitespace: true,
-                        },
-                      ]}
-                    >
-                      <Input style={{ width: "22rem", marginLeft: "2.2rem" }} />
-                    </Form.Item>
-                    <Form.Item label="Desciption" name="description">
-                      <TextArea
-                        rows={4}
-                        style={{ width: "22rem", marginLeft: "1.6rem" }}
-                      />
-                    </Form.Item>
-                    <Form.Item
-                      name="product_price"
-                      label="Price"
-                      rules={[
-                        {
-                          message: "Please input your price!",
-                          whitespace: true,
-                        },
-                      ]}
-                    >
-                      <Input style={{ width: "10rem", marginLeft: "4rem" }} />
-                    </Form.Item>
-    
-                    <Form.Item
-                      valuePropName="checked"
-                      wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                      }}
-                      name="draft"
-                    >
-                      <Checkbox style={{ paddingTop: "1rem" }}>
-                        <p style={{ paddingTop: "2.8px" }}>Add to Draft</p>{" "}
-                      </Checkbox>
-                    </Form.Item>
-    
-                    <Form.Item {...tailFormItemLayout}>
-                      <Button
-                        style={{
-                          width: "100px",
-                          backgroundColor: "black",
-                          paddingLeft: "1rem",
-                          height: "3rem",
-                          borderRadius: "1rem",
-                          marginRight: "1rem",
-                        }}
-                        onClick={handleAdd}
-                        type="primary"
-                        htmlType="submit"
-                      >
-                        Edit
-                      </Button>
-                    </Form.Item>
+        {realStore.map((e, i) => {
+          if (e.name !== "") 
+            return (
+              <div key={i}>
+                <div className="box">
+                  <img
+                    src="https://i.pinimg.com/originals/a3/29/66/a32966c2ab5ba98a7332a72eb1953c48.jpg"
+                    alt=""
+                    className="imgca"
+                  />
+                  <div className="hash">
+                    <h3>{e.name}</h3>
+                    <h4>₹{e.price}</h4>
                   </div>
-                </Form>
-              </Modal>
-                <button onClick={()=>handleDelete(i,"realStore")}>Delete</button>
+
+                  <p>{e.description}</p>
+                  <button onClick={() => openAdd(i, "realStore")}>Edit</button>
+                  <Modal
+                    title="Add Product Details"
+                    open={isaddopen}
+                    onOk={handleAddOk}
+                    footer={null}
+                    onCancel={handleAddCancel}
+                    style={{ paddingTop: "2rem" }}
+                  >
+                    <Form
+                      name="normal_login"
+                      className="login-form"
+                      form={form}
+                      initialValues={{
+                        remember: true,
+                      }}
+                      onFinish={(values) => handleAdd(values)}
+                    >
+                      <div className="addcontainer">
+                        <Form.Item
+                          label="Image      "
+                          valuePropName="fileList"
+                          getValueFromEvent={normFile}
+                        >
+                          {" "}
+                          <center>
+                            <Upload action="/upload.do" listType="picture-card">
+                              <div>
+                                <PlusOutlined />
+                                <div
+                                  style={{
+                                    marginTop: 8,
+                                  }}
+                                >
+                                  Upload
+                                </div>
+                              </div>
+                            </Upload>
+                          </center>
+                        </Form.Item>
+                        <Form.Item
+                          name="product_name"
+                          label="Name"
+                          tooltip="What do you want others to call you?"
+                          rules={[
+                            {
+                              message: "Please input your nickname!",
+                              whitespace: true,
+                            },
+                          ]}
+                        >
+                          <Input
+                            style={{ width: "22rem", marginLeft: "2.2rem" }}
+                          />
+                        </Form.Item>
+                        <Form.Item label="Desciption" name="description">
+                          <TextArea
+                            rows={4}
+                            style={{ width: "22rem", marginLeft: "1.6rem" }}
+                          />
+                        </Form.Item>
+                        <Form.Item
+                          name="product_price"
+                          label="Price"
+                          rules={[
+                            {
+                              message: "Please input your price!",
+                              whitespace: true,
+                            },
+                          ]}
+                        >
+                          <Input
+                            style={{ width: "10rem", marginLeft: "4rem" }}
+                          />
+                        </Form.Item>
+
+                        <Form.Item
+                          valuePropName="checked"
+                          wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                          }}
+                          name="draft"
+                        >
+                          <Checkbox style={{ paddingTop: "1rem" }}>
+                            <p style={{ paddingTop: "2.8px" }}>Add to Draft</p>{" "}
+                          </Checkbox>
+                        </Form.Item>
+
+                        <Form.Item {...tailFormItemLayout}>
+                          <Button
+                            style={{
+                              width: "100px",
+                              backgroundColor: "black",
+                              paddingLeft: "1rem",
+                              height: "3rem",
+                              borderRadius: "1rem",
+                              marginRight: "1rem",
+                            }}
+                            onClick={handleAdd}
+                            type="primary"
+                            htmlType="submit"
+                          >
+                            Edit
+                          </Button>
+                        </Form.Item>
+                      </div>
+                    </Form>
+                  </Modal>
+                  <button onClick={() => handleDelete(i, "realStore")}>
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          );}
+            );
+          
         })}
       </div>
-            <h1 style={{ paddingTop: "2rem" }}> Drafts</h1>
+      <h1 style={{ paddingTop: "2rem" }}> Drafts</h1>
       <div className="boxes b1">
         {myDraft.map((e, i) => {
+          if (e.name !== "") {
+            return (
+              <div key={i}>
+                <div className="box">
+                  <img
+                    src="https://i.pinimg.com/originals/a3/29/66/a32966c2ab5ba98a7332a72eb1953c48.jpg"
+                    alt=""
+                    className="imgca"
+                  />
+                  <div className="hash">
+                    <h3>{e.name}</h3>
+                    <h4>₹{e.price}</h4>
+                    <button></button>
+                  </div>
 
-          if (e.name !== ""){
-          return (
-            <div key={i}>
-              <div className="box">
-                <img
-                  src="https://i.pinimg.com/originals/a3/29/66/a32966c2ab5ba98a7332a72eb1953c48.jpg"
-                  alt=""
-                  className="imgca"
-                />
-                <div className="hash">
-                  <h3>{e.name}</h3>
-                  <h4>₹{e.price}</h4>
-                  <button></button>
+                  <p>{e.description}</p>
+                  <button onClick={() => openAdd(i, "draft")}>Edit</button>
+                  <button onClick={() => handleDelete(i, "draft")}>
+                    Delete
+                  </button>
                 </div>
-
-                <p>{e.description}</p>
-                <button onClick={()=>handleDelete(i,"draft")}>Delete</button>
               </div>
-            </div>
-          );}
+            );
+          }
         })}
       </div>
     </>
